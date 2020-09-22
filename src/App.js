87280613908a666d5048a66f746873042b3eb897
dmajkowski <dmajkowski@onet.pl>
2020-09-22 18:07:29 +0200

    added search logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./App.css";
+import Search from "./components/Search";
 
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {},
+  });
+  const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=8e8bc6db";
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${apiUrl}&s=${state.s}`)
+        .then((response) => response.json())
+        .then((data) => {
+          let results = data.Search;
+          setState((prevState) => {
+            return { ...prevState, results };
+          });
+        });
+    }
+  };
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState((prevState) => {
+      return { ...prevState, s: s };
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">New app</header>
+    <div className="app">
+      <header className="app-header">
+        <h1>Movie Database</h1>
+      </header>
+      <main className="app-main">
+        <Search handleInput={handleInput} search={search} />
+      </main>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Search.css";
+
+function Search({ handleInput, search }) {
+  return (
+    <section className="searchbox-wrap">
+      <input
+        type="text"
+        placeholder="Search for a movie..."
+        className="searchbox"
+        onChange={handleInput}
+        onKeyPress={search}
+      />
+    </section>
+  );
+}
+
+export default Search;
