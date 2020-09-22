3f54b8e67542fd8112646403fa5a1d50ea26da9f
dmajkowski <dmajkowski@onet.pl>
2020-09-22 18:36:10 +0200

    results component ready

---
+++ src/components/Results.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Results({ results }) {
+  return (
+    <section className="results">
+      {/* {results.map((result) => (
+        <Result result={result} />
+      ))} */}
+      Results here
+    </section>
+  );
+}
+
+export default Results;
