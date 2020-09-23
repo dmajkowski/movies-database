7e1ba1f31278098cf0145dd05751b285b9c5eb32
dmajkowski <dmajkowski@onet.pl>
2020-09-23 17:24:14 +0200

    added popup styling

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Popup.css";
 
 function Popup({ selectedDetails, handleClosePopup }) {
   return (
     <section className="popup">
       <div className="content">
-        <h3>
-          {selectedDetails.Title} <span>{selectedDetails.Year}</span>
-        </h3>
+        <h2>
+          {selectedDetails.Title} <span>({selectedDetails.Year})</span>
+        </h2>
         <p className="rating">Rating: {selectedDetails.imdbRating}</p>
         <div className="plot">
           <img
@@ -15,10 +16,10 @@ function Popup({ selectedDetails, handleClosePopup }) {
             className="poster"
           />
           <p className="description">{selectedDetails.Plot}</p>
-          <button className="closePopup-btn" onClick={handleClosePopup}>
-            Close
-          </button>
         </div>
+        <button className="closePopup-btn" onClick={handleClosePopup}>
+          Close
+        </button>
       </div>
     </section>
   );
